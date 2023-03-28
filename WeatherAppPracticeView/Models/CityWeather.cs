40617a88ca82188a05b6aeae6eaf02725a339375
An <annogo123@gmail.com>
2023-03-28 22:25:12 +0700

    Add project files.

---
+++ WeatherAppPracticeView/Models/CityWeather.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherAppPracticeView.Models
+{
+    public class CityWeather
+    {
+        public string? CityUniqueCode { get; set; }
+        public string? CityName { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public int TemperatureFahrenheit { get; set; }
+
+        public string HoursAmPm  => DateAndTime.ToString("HH:mm tt");
+
+        public string BackgroundColorByTemperature => TemperatureFahrenheit > 74 ? "green-back" :
+                                                      TemperatureFahrenheit < 44 ? "blue-back" : "orange-back";
+
+    }
+}
